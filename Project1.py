6d35559cf42d715a23479f33e5bd561b52fdc9af
AkshayKurhade <akurhade@umd.edu>
2020-02-16 16:29:52 -0500

    Initial state read

--- Project1.py
+++ Project1.py
@@ -1,3 +1,13 @@
 import numpy as np
 
-print("initial test")
+Node_state = [[0, 0]]  # storing parent child state info
+
+Final_node_state = []   # Final state info
+
+initial_state = []  # matrix to store initial configuration
+print("enter the initial state list")
+for i in range(3):
+    initial_state.append(list(map(int, input().rstrip().split())))
+
+start_config = np.array(initial_state)
+print(initial_state)
