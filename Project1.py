8e9648029f792148694fec3870ffe22ee4b1ef63
AkshayKurhade <akurhade@umd.edu>
2020-02-16 17:29:24 -0500

    Blank tile recognition

--- Project1.py
+++ Project1.py
@@ -6,7 +6,7 @@ Final_node_state = []  # Final state info
 
 initial_state = []  # matrix to store initial configuration
 print("enter the initial state list")
-for i in range(3):
+for x in range(3):
     initial_state.append(list(map(int, input().rstrip().split())))
 
 start_config = np.array(initial_state)
@@ -15,6 +15,49 @@ print(initial_state)
 
 # finding blank tile location
 
-def blanktile_location(at_node):
-    row, col = np.where(at_node == 0)
+def BlankTileLocation(CurrentNode):
+    row, col = np.where(CurrentNode == 0)
     return row, col
+
+
+def ActionMoveLeft(CurrentNode):
+    newNode = np.copy(CurrentNode)
+    [i, j] = BlankTileLocation(newNode)
+    if j == 0:
+        status = False
+    else:
+        status = True
+    newNode[[i, j]], newNode[[i, j-1]] = newNode[[i, j-1]], newNode[[i, j]]
+    return status, newNode
+
+def ActionMoveRight(CurrentNode):
+    newNode = np.copy(CurrentNode)
+    [i, j] = BlankTileLocation(newNode)
+    if j == 2:
+        status = False
+    else:
+        status = True
+    newNode[[i, j]], newNode[[i, j+1]] = newNode[[i, j+1]], newNode[[i, j]]
+    return status, newNode
+
+def ActionMoveUp(CurrentNode):
+    newNode = np.copy(CurrentNode)
+    [i, j] = BlankTileLocation(newNode)
+    if i == 0:
+        status = False
+    else:
+        status = True
+    newNode[[i, j]], newNode[[i-1, j]] = newNode[[i-1, j]], newNode[[i, j]]
+    return status, newNode
+
+def ActionMoveDown(CurrentNode):
+    newNode = np.copy(CurrentNode)
+    [i, j] = BlankTileLocation(newNode)
+    if i == 2:
+        status = False
+    else:
+        status = True
+    newNode[[i, j]], newNode[[i+1, j]] = newNode[[i+1, j]], newNode[[i, j]]
+    return status, newNode
+
+
