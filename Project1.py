42200683caeb8966d54fe763796c5a254c3e31de
AkshayKurhade <akurhade@umd.edu>
2020-02-21 10:05:27 -0500

    Move Functions optimisation conversion functions writing simultaneously new way of traversing the queue

--- Project1.py
+++ Project1.py
@@ -1,8 +1,12 @@
 import numpy as np
 import os
-import warnings
+import collections
 
-warnings.filterwarnings(action='ignore', category=FutureWarning)
+info_file = open("NodesInfo.txt", "a+")
+nodes_file = open("Nodes.txt", "a+")
+# import warnings
+#
+# warnings.filterwarnings(action='ignore', category=FutureWarning)
 
 Node_state = [[0, 0]]  # storing parent child state info
 
@@ -16,10 +20,31 @@ final_state = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])  # final puzzle config
 print("enter the initial state list")
 for x in range(3):
     initial_state.append(list(map(int, input().rstrip().split())))
-
+# initial_state = [[1, 7, 3], [5, 8, 2], [6, 0, 4]]
 start_config = np.array(initial_state)
-
 testarray = start_config.flatten()
+
+
+def mattostring(testarray):
+    i = 0
+    nospace = ""  # Create an empty string which will be the single line print out
+    for col in range(0, 3):
+        for row in range(0, 3):
+            nospace = nospace + str(testarray[row, col])
+            i += 1
+    return nospace
+
+
+def stringtomat(nonspaced_str):
+    nonspaced_str = list(nonspaced_str)
+    i = 0
+    mat = np.empty([3, 3])
+    mat = mat.astype('int')
+    for col in range(0, 3):
+        for row in range(0, 3):
+            mat[row, col] = int(nonspaced_str[i])
+            i += 1
+    return mat
 
 
 # finding blank tile location
@@ -30,52 +55,68 @@ def BlankTileLocation(CurrentNode):
 
 # Move Blank Tile to Left
 def ActionMoveLeft(CurrentNode):
+    CurrentNode = stringtomat(CurrentNode)
     newNode = np.copy(CurrentNode)
     [i, j] = BlankTileLocation(newNode)
     if j == 0:
         status = False
+        return
     else:
         status = True
-    newNode[[i, j]], newNode[[i, j - 1]] = newNode[[i, j - 1]], newNode[[i, j]]
-    return status, newNode
+        newNode[i, j] = CurrentNode[i, j - 1]
+        newNode[i, j - 1] = 0
+        newNode = mattostring(newNode)
+    return newNode
 
 
 # Move Blank Tile to Right
 def ActionMoveRight(CurrentNode):
+    CurrentNode = stringtomat(CurrentNode)
     newNode = np.copy(CurrentNode)
     [i, j] = BlankTileLocation(newNode)
-    if j < 2:
-        status = True
-        newNode[[i, j]], newNode[[i, j + 1]] = newNode[[i, j + 1]], newNode[[i, j]]
-    else:
+    if j == 2:
         status = False
+        return
+    else:
+        status = True
+        newNode[i, j] = CurrentNode[i, j + 1]
+        newNode[i, j + 1] = 0
+        newNode = mattostring(newNode)
 
-    return status, newNode
+    return newNode
 
 
 # Move Blank Tile UP
 def ActionMoveUp(CurrentNode):
+    CurrentNode = stringtomat(CurrentNode)
     newNode = np.copy(CurrentNode)
     [i, j] = BlankTileLocation(newNode)
     if i == 0:
         status = False
+        return
     else:
         status = True
-    newNode[[i, j]], newNode[[i - 1, j]] = newNode[[i - 1, j]], newNode[[i, j]]
-    return status, newNode
+        newNode[i, j] = CurrentNode[i - 1, j]
+        newNode[i - 1, j] = 0
+        newNode = mattostring(newNode)
+    return newNode
 
 
 # Move Blank Tile DOWN
 def ActionMoveDown(CurrentNode):
+    CurrentNode = stringtomat(CurrentNode)
     newNode = np.copy(CurrentNode)
     [i, j] = BlankTileLocation(newNode)
-    if i < 2:
-        status = True
-        newNode[[i, j]], newNode[[i + 1, j]] = newNode[[i + 1, j]], newNode[[i, j]]
-    else:
+    if i == 2:
         status = False
+        return
+    else:
+        status = True
+        newNode[i, j] = CurrentNode[i + 1, j]
+        newNode[i + 1, j] = 0
+        newNode = mattostring(newNode)
 
-    return status, newNode
+    return newNode
 
 
 def CheckNodePresence(testnode, list):
@@ -98,126 +139,80 @@ def IsPuzzleSolvable(puzzletest):
     return inv_count % 2 == 0
 
 
+def WriteNode(flatarray):
+    temp_string = ""
+    for i in range(0, 9):
+        temp_string = temp_string + str(flatarray[i]) + " "
+    temp_string = temp_string + "\n"
+    nodes_file.write(temp_string)
+
+
+def WriteNodeInfo(child_index, parent_index):
+    write_row = str(child_index) + " " + str(parent_index) + " " + str(0) + "\n"
+    info_file.write(write_row)
+
+
 a = IsPuzzleSolvable(testarray) == 1
 
 if a == 1:
-    a = 0
     print(" Puzzle is solvable\n Solving the puzzle....\n Please be patient this may take a while..")
-
+    board_tracker = set()
     parent_index = 0
     child_index = 0
-    start_node = start_config
-    Final_node_state.append(start_node)
+    nodes_list = []
+    # start_node = start_config
+    start_node = mattostring(start_config.copy())
+    nodes_list.append([start_node, -1])
+    queue = collections.deque()
+    queue.append(0)
+    i = 0
+    temp = np.empty(4, dtype='object')
+    reached_finalstate = False
+    # Final_node_state.append(start_node)
+    live_node = ""
+    while queue:
+        parent_index = queue.popleft()
+        live_node = nodes_list[parent_index][0]
+        temp[0] = ActionMoveLeft(live_node)
+        temp[1] = ActionMoveRight(live_node)
+        temp[2] = ActionMoveUp(live_node)
+        temp[3] = ActionMoveDown(live_node)
 
-    while parent_index <= len(Final_node_state):
-        live_node = Final_node_state[parent_index]
+        for b in range(0, 4):
+            # Don't want empty entries and don't want any repeat entries
+            if (temp[b] is not None) and (temp[b] not in board_tracker):
+                board_tracker.add(temp[b])  # add it to the board_tracker
+                WriteNode(temp[b])
+                WriteNodeInfo(len(nodes_list) - 1, parent_index)
+                nodes_list.append([temp[b], parent_index])
+                queue.append(len(nodes_list) - 1)
 
-        status, left_temp = ActionMoveLeft(live_node)
-        if status == 1:
-            if CheckNodePresence(left_temp, Final_node_state) == 0:
-                Final_node_state.append(left_temp)
-                child_index += 1
-        Node_state.append([parent_index, child_index])
-
-        status, right_temp = ActionMoveRight(live_node)
-        if status == 1:
-            if CheckNodePresence(right_temp, Final_node_state) == 0:
-                Final_node_state.append(right_temp)
-                child_index += 1
-        Node_state.append([parent_index, child_index])
-
-        status, up_temp = ActionMoveUp(live_node)
-        if status == 1:
-            if CheckNodePresence(up_temp, Final_node_state) == 0:
-                Final_node_state.append(up_temp)
-                child_index += 1
-        Node_state.append([parent_index, child_index])
-
-        status, down_temp = ActionMoveDown(live_node)
-        if status == 1:
-            if CheckNodePresence(down_temp, Final_node_state) == 0:
-                Final_node_state.append(down_temp)
-                child_index += 1
-        Node_state.append([parent_index, child_index])
-
-        parent_index += 1
-
-        if np.array_equal(left_temp, final_state):
-            print(left_temp)
-            break
-        if np.array_equal(right_temp, final_state):
-            print(right_temp)
-            break
-        if np.array_equal(up_temp, final_state):
-            print(up_temp)
-            break
-        if np.array_equal(down_temp, final_state):
-            print(down_temp)
-            break
-
-    lists = []
-    final_state = final_state.transpose()
-    lists.append(final_state)
-    NodeState_length = len(Node_state)
-    Node_state = np.array(Node_state)
-
-    X = Node_state[NodeState_length - 1]
-    element1 = X[0]
-    element2 = X[1]
-    print(element2, element1)
-    count = 0
-    while element1 != 0:
-        for i in range(NodeState_length):
-            X = Node_state[i]
-            if X[1] == element1:
-                element1 = X[0]
-                element2 = X[1]
-                Y = Final_node_state[element2]
-                Y = np.array(Y)
-                Y = Y.transpose()
-                listY = Y.tolist()
-                lists.append(listY)
-                count += 1
-                print(element2, element1)
-
-    start_node = np.array(start_node)
-    start_node = start_node.transpose()
-    lists.append(start_node)
-
-    if os.path.exists("Nodes.txt"):
-        os.remove("Nodes.txt")
-
-    file = open("Nodes.txt", "a")
-    X = Final_node_state
-    for item in X:
-        for element in item.T:
-            for index in element:
-                file.write("%i\t" % index)
-        file.write("\n")
-    file.close()
-
-    if os.path.exists("NodesInfo.txt"):
-        os.remove("NodesInfo.txt")
-
-    file = open("NodesInfo.txt", "a")
-    Z = Node_state.tolist()
-    for item in Z:
-        for index in reversed(range(2)):
-            file.write("%i\t" % item[index])
-        file.write("0")
-        file.write("\n")
-    file.close()
-
+                if (temp[b] == mattostring(final_state)):
+                    goal_node = len(nodes_list)  # -1 #-1 to compensate for 0 index
+                    # Clear everything else so the program stops here
+                    queue.clear()
+                    reached_finalstate = True
+                    break
+            if reached_finalstate:
+                break
+    print("Path Found..\n Wrapping Up")
+    Final_node_state.append(nodes_list[-1][0])  # Goal
+    parent = nodes_list[-1][1]  # parent to the goal
+    while (parent != -1):
+        Final_node_state.append(nodes_list[parent][0])
+        parent = nodes_list[parent][1]
+    # Reverse the path so its from start to goal
+    Final_node_state.reverse()
     if os.path.exists("nodePath.txt"):
         os.remove("nodePath.txt")
     file = open("nodePath.txt", "w")
-    for a in reversed(lists):
-        for element in a:
-            for index in element:
-                file.write("%i\t" % index)
-        file.write("\n")
-    file.close()
+    for count, move in enumerate(Final_node_state):
+        emptystring = ""
+        for i in range(0, len(move)):
+            emptystring = emptystring + str(move[i]) + " "
+        emptystring = emptystring + "\n"
+        file.write(emptystring)
 
-    print("no of nodes created: ", len(Final_node_state))
+
 else:
     print("Puzzle is unsolvable")
